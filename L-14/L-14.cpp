46961a149ca4b87cbe9002879d05a5cb790b3e80
Vasya-556 <vkobrin556@gmail.com>
2023-04-11 20:03:58 +0300

    first commit

---
+++ L-14/L-14.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream> // std::cout
+#include <iostream> // std::cout
+#include <list> // std::list
+#include <algorithm> // std::generate
+#include <ctime> // std::time
+#include <numeric> // std::accumulate
+
+using namespace std;
+
+int RandomNumber() { return rand() % 100; }
+void print(list<int> a)
+{
+	for (int i : a)
+		cout << i << " ";
+	cout << endl;
+}
+int main()
+{
+	srand((unsigned int)time(NULL));
+	int n;
+	cout << "input n : "; cin >> n;
+	list<int> l(n);
+	generate(l.begin(), l.end(), RandomNumber);
+	print(l);
+	cout << "count = " << count_if(l.begin(), l.end(), [](int i) {return i % 2; }) << endl;
+	return 0;
+}
